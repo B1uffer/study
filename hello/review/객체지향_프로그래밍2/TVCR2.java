1f6041b4e128f4cbf9d8789fc86b9922bc75ee56
B1uffer <135810601+B1uffer@users.noreply.github.com>
2024-01-14 23:49:39 +0900

    24_01_14

--- hello/review/객체지향_프로그래밍2/TVCR2.java
+++ hello/review/객체지향_프로그래밍2/TVCR2.java
@@ -2,6 +2,9 @@ package 객체지향_프로그래밍2;
 
 public class TVCR2 extends Tv4 implements IVCR {
 	
+	// 다중상속의 경우 두 클래스 이상을 상속받고 싶을 경우
+	// 한쪽만 상속받고 나머지 한 쪽은 클래스 내에 포함시켜서 내부적으로 인스턴스를 생성 후 사용함
+	
 	VCR2 vcr = new VCR2();
 	
 	public void play() {
