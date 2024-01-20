dd7705405de7ad1b79f2bcdaae50d8cb0d137451
B1uffer <135810601+B1uffer@users.noreply.github.com>
2024-01-20 18:20:38 +0900

    24_01_20

--- hello/review/객체지향_프로그래밍2/ex32_내부클래스2.java
+++ hello/review/객체지향_프로그래밍2/ex32_내부클래스2.java
@@ -2,5 +2,20 @@ package 객체지향_프로그래밍2;
 
 public class ex32_내부클래스2 {
 	
+	class InstanceInner {
 	
+	}
+	
+	static class StaticInner {
+		
+	}
+	
+	InstanceInner iv = new InstanceInner();
+	
+	static StaticInner cv = new StaticInner();
+	
+	
+	static void staticMethod() {
+		
+	}
 }
