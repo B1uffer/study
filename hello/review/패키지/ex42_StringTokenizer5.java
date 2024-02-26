7a06de828f15021e80e29ea27b976de758d7be99
B1uffer <135810601+B1uffer@users.noreply.github.com>
2024-02-26 16:01:31 +0900

    24_02_26

--- hello/review/패키지/ex42_StringTokenizer5.java
+++ hello/review/패키지/ex42_StringTokenizer5.java
@@ -7,6 +7,8 @@ public class ex42_StringTokenizer5 {
 	public static void main(String[] args) {
 		
 		// 구분자를 ','로 하는 문자열 데이터를 String 클래스의 split()과 StringTokenizer로 잘라서 결과를 비교하는 예제
+		// split()은 빈 문자열도 토큰으로 인식한다
+		// StringTokenizer는 빈 문자열을 토큰으로 인식하지 않는다
 		
 		String data = "100,,,200,300";
 		
