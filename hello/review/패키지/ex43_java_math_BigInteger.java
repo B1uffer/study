1e0cb7d1c4f946b64845d6f18fb4bf8dde81c032
B1uffer <135810601+B1uffer@users.noreply.github.com>
2024-02-28 22:35:07 +0900

    24_02_28

--- hello/review/패키지/ex43_java_math_BigInteger.java
+++ hello/review/패키지/ex43_java_math_BigInteger.java
@@ -4,9 +4,25 @@ import java.math.*;
 
 public class ex43_java_math_BigInteger {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		
+		for(int i=1; i<100; i++) {
+			
+			System.out.printf("%d != %s%n", i, calcFactorial(i));
+			
+		}
 		
 	}
-}
+	
+	static String calcFactorial(int n) {
+		
+		return
+		
+	}
+	
+	static BigInteger factorial(BigInteger n) {
+		
+		return
+		
+	}
+} 
