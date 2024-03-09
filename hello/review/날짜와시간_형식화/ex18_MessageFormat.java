705a8b459067d15adcd91d2b8df2af99eddc8b03
B1uffer <135810601+B1uffer@users.noreply.github.com>
2024-03-09 23:42:03 +0900

    24_03_09

--- hello/review/날짜와시간_형식화/ex18_MessageFormat.java
+++ hello/review/날짜와시간_형식화/ex18_MessageFormat.java
@@ -7,7 +7,12 @@ public class ex18_MessageFormat {
 	public static void main(String[] args) {
 		
 		String msg = "Name : {0} \nTel : {1} \nAge : {2}";
-
+		
+		Object[] arguments = {"이자바", "02-123-1234", "27", "07-09"};
+		
+		String result = MessageFormat.format(msg, arguments);
+		System.out.println(result);
+		
 	}
 
 }
