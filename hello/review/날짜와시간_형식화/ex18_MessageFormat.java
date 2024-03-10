170a3b380eb793fb399dd5ad5e6afcaa5019c98e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2024-03-10 23:57:24 +0900

    24_03_10

--- hello/review/날짜와시간_형식화/ex18_MessageFormat.java
+++ hello/review/날짜와시간_형식화/ex18_MessageFormat.java
@@ -7,6 +7,9 @@ public class ex18_MessageFormat {
 	public static void main(String[] args) {
 		
 		String msg = "Name : {0} \nTel : {1} \nAge : {2}";
+		// msg = "";에서 {0}, {1}, {2}로 표시된 부분이 데이터가 출력될 자리임
+		// 순차적일 필요는 없고 여러 번 반복해서 사용할 수도 있다
+		// 여기서 사용되는 숫자는 배열마냥 0부터 시작해야하며 양식에 들어갈 데이터는 밑의 Object[] arguments에 지정되어 있다
 		
 		Object[] arguments = {"이자바", "02-123-1234", "27", "07-09"};
 		
---
+++ hello/review/날짜와시간_형식화/ex19_MessageFormat2.java
@@ -0,0 +1,16 @@
+package 날짜와시간_형식화;
+
+import java.text.*;
+
+public class ex19_MessageFormat2 {
+
+	public static void main(String[] args) {
+		
+		// 반복문으로 하나 이상의 데이터 집합을 출력하는 예제
+		
+		String tableName = "CUST_INFO";
+		String msg = "INSERT INTO " + tableName + "VALUES ({0}, {1}, {2}, {3})";
+		
+	}
+
+}
