98beb131edf6451d42aa9ac0b325c022bd235a0e
B1uffer <135810601+B1uffer@users.noreply.github.com>
2024-11-08 13:32:11 +0900

    Add files via upload

---
+++ HTML_CSS_JS/1108/query.sql
@@ -0,0 +1,10 @@
+create table information (
+	
+	num int not null auto_increment,
+	wid char not null,
+	wpassword int(20) not null,
+	wname char(10) not null,
+	
+	primary key(num)
+	
+);
---
+++ HTML_CSS_JS/1108/sub1.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>무제 문서</title>
+</head>
+
+<body>
+	
+	<?php include 'header.php'; ?>
+	
+	서브페이지 메인
+	
+	<?php include 'footer.php'; ?>
+	
+	<script>
+		
+		
+		
+	</script>
+	
+</body>
+	
+
+</html>
